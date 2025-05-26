a964488b867a2778cff669d6848179b41c872855
KartikMaski <kartikmaski29032004@gmail.com>
2025-05-26 21:16:01 +0530

    commit

--- agents/scraping_agent.py
+++ agents/scraping_agent.py
@@ -1,4 +1,5 @@
 # scraping_agent.py
-from data_ingestion.earnings_scraper import get_earnings_surprise
+
+from data_ingestion.earning_scraper import get_earnings_surprise
 from data_ingestion.filings_loader import download_latest_10k
- 
+
